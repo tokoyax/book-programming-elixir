ed558de396332f96504526ebacd4ed981e0af594
tokoyax <getting.foolish@gmail.com>
2016-09-24 14:20:39 +0900

    試行錯誤中

--- practice/modules_and_functions_6.exs
+++ practice/modules_and_functions_6.exs
@@ -1,35 +1,24 @@
 defmodule Chop do
 
-  def guess(actual, min..max) when actual == min, do: IO.puts min
-
-  def guess(actual, min..max) when actual == max, do: IO.puts max
-
   def guess(actual, min..max) when min < actual and actual < max do
-    estimate = div(max, 2)
-    IO.puts "Is it #{estimate}"
-    IO.puts "#{actual} #{estimate} #{min}..#{max}"
-    guess(actual, estimate, min..max)
+    IO.puts "Is it #{div(max, 2)}"
+    guess(actual, div(max, 2), min..max)
   end
 
-  def guess(actual, estimate, min..max) when estimate == actual or estimate == min or estimate == max do
+  def guess(actual, estimate, min..max) when actual == estimate or actual == min or actual == max do
     IO.puts actual
   end
 
   def guess(actual, estimate, min..max) when estimate < actual do
-    min = estimate + 1
-    estimate = estimate + div(max-estimate, 2)
-    IO.puts "Is it #{estimate}"
-    IO.puts "#{actual} #{estimate} #{min}..#{max}"
-    guess(actual, estimate, min..max)
+    IO.puts "A: Is it #{estimate + div(max - estimate, 2)}"
+    guess(actual, estimate + div(max - estimate, 2), estimate+1..max)
   end
 
   def guess(actual, estimate, min..max) when actual < estimate do
-    max = estimate - 1
-    estimate = div(estimate, 2)
-    IO.puts "Is it #{estimate}"
-    IO.puts "#{actual} #{estimate} #{min}..#{max}"
-    guess(actual, estimate, min..max)
+    IO.puts "B: Is it #{div(estimate, 2)}"
+    guess(actual, div(estimate, 2), min..estimate-1)
   end
+
 end
 
 Chop.guess(273, 1..1000)
